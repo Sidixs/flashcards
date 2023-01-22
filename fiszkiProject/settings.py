36fb5bdc19935b676b77c073af75a5b60f325ac0
Sidixs <sylwesterkowal2000@gmail.com>
2023-01-22 17:16:54 +0100

    test

--- fiszkiProject/settings.py
+++ fiszkiProject/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = 'django-insecure-4^eq@w=jc@@bv8kb*gt9i1mqsl2c22f(*9*luj$+oz71m1ru@a
 DEBUG = True
 # ALLOWED_HOSTS = []
 
-#DEBUG = False
-ALLOWED_HOSTS = ['flashcards-st-pro.azurewebsites.net']
-CSRF_TRUSTED_ORIGINS = ['https://flashcards-st-pro.azurewebsites.net']
+DEBUG = False
 
 # Application definition
 
